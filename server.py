0f170844b711508b1ad6ebdd1f88f052a549a95e
Gabriel.Gontijo <38219914+GGontijo@users.noreply.github.com>
2023-09-20 23:18:42 -0400

    changing structure

--- server.py
+++ server.py
@@ -7,50 +7,48 @@ from web.map_helper import map_helper
 from flask import jsonify
 from threading import Thread
 
-app = Flask(__name__, template_folder="web")
-
-db =  DbHelper()
-
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] - %(message)s')
 
-class Server:
-    def __init__(self) -> None:
-        self.telegram_service = Telegram_Service(db)
-        self.vuln = Vuln(db)
-        self.map = map_helper(db)
+app = Flask(__name__, template_folder="web")
+db =  DbHelper()
+vuln = Vuln(db)
+map = map_helper(db)
+telegram_service = Telegram_Service(db)
 
-    def start_api(self):
-        app.run(host="0.0.0.0")
-
-    def start(self):
-        api_thread = Thread(target=self.start_api)
-        api_thread.start()
-        self.telegram_service.process_messages()
+def start_api():
+    app.run(host="0.0.0.0")
+    
+def start_server():
+    api_thread = Thread(target=start_api)
+    api_thread.start()
+    telegram_service.process_messages()
     
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-    
-    
-    @app.route('/compile')
-    def compile():
-        return 'Lá ele!'
-        #ssid = request.args.get('ssid')
-        #mac = request.args.get('mac')
-        #if ssid and mac:
-        #    logging.info(f"Compilando para o SSID: {ssid}, MAC: {mac}")
-        #    return 
-        #else:
-        #    return "Parâmetros 'ssid' e 'mac' não fornecidos corretamente na URL."
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-    @app.route('/list')
-    def get_all_vulns():
-        access_points = db.get_ap_all()
-        ap_list = [ap.dict() for ap in access_points]
-        return jsonify(ap_list)
+@app.route('/compile')
+def compile():
+    return 'Lá ele!'
+    #ssid = request.args.get('ssid')
+    #mac = request.args.get('mac')
+    #if ssid and mac:
+    #    logging.info(f"Compilando para o SSID: {ssid}, MAC: {mac}")
+    #    return 
+    #else:
+    #    return "Parâmetros 'ssid' e 'mac' não fornecidos corretamente na URL."
+
+@app.route('/list')
+def get_all_vulns():
+    access_points = db.get_ap_all()
+    ap_list = [ap.dict() for ap in access_points]
+    return jsonify(ap_list)
+
+@app.route('/updatemap')
+def update_map():
+    return vuln.check_vuln_pending_db()
 
 if __name__ == '__main__':
-    s = Server()
-    s.start()
+    start_server()
